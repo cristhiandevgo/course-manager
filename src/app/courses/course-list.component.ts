d784901ca1c48b9a932d2a3d54fd62e7f9887f61
cristhiandevgo <ivancristhian@hotmail.com>
2022-07-24 14:43:20 -0300

    Add: Dependency injection / Injeção de Dependência

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Course } from "./course";
+import { CourseService } from "./star/course.service";
 
 @Component({
 	selector: "app-course-list",
@@ -10,28 +11,11 @@ import { Course } from "./course";
 export class CourseListComponent implements OnInit{
 	courses: Course[] = [];
 
+	constructor(private courseService: CourseService) {
+
+	}
+
 	ngOnInit(): void{
-		this.courses = [
-			{
-				id: 1,
-				name: "Angular: Forms",
-				imageUrl: "./assets/images/forms.png",
-				price: 99.99,
-				code: "ABC-123",
-				duration: 120,
-				rating: 5,
-				releaseDate: "July, 23, 2022"
-			},
-			{
-				id: 2,
-				name: "Angular: HTTP",
-				imageUrl: "./assets/images/http.png",
-				price: 90,
-				code: "ABC-123",
-				duration: 120,
-				rating: 4.5,
-				releaseDate: "July, 23, 2022"
-			}
-		]
+		this.courses = this.courseService.retrieveAll();
 	}
 }
